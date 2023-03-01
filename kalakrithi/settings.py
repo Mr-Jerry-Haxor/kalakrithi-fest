a5e73c724efc25fe90c3e0a2ef3b17d6b61f5f28
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2023-03-01 14:09:31 +0000

    verify details border

--- kalakrithi/settings.py
+++ kalakrithi/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i55qf&58!+psd%lw=oijfrq5q2jkcw@%k7lazatk*$)w*nzkv_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
   "e813f790-f545-407f-a989-ab8e69f3b547.id.repl.co",
